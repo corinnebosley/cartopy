d1d566878b2cd09d46cae4bd1d9883630a8b4e79
Corinne Bosley <corinne.e.bosley@gmail.com>
2017-12-18 16:35:39 +0000

    capitalized Matplotlib in other files

--- lib/cartopy/examples/un_flag.py
+++ lib/cartopy/examples/un_flag.py
@@ -16,7 +16,7 @@ filled_land = True
 
 def olive_path():
     """
-    Returns a matplotlib path representing a single olive branch from the
+    Returns a Matplotlib path representing a single olive branch from the
     UN Flag. The path coordinates were extracted from the SVG at
     https://commons.wikimedia.org/wiki/File:Flag_of_the_United_Nations.svg.
 
